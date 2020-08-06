612b5052d1b12b6d9236c81810caa96d60bfd5f9
landaverde1196 <alejndro1196@gmail.com>
2020-08-06 13:19:27 -0600

    deploy

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Munchies</h1>
         <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={this.state.businesses} />
       </div>
